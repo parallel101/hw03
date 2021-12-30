fce3f379f099fe4dbb61bb312368047256176229
lzh <12132276@mail.sustech.edu.cn>
2021-12-30 15:23:46 +0800

    fix(main): 1.补充 operate<< 模板的定义。 2.逐元素相加，Vector的结果由长度短的Vector决定。 3.std::visit现实加法，但是main中为variant + vector，所以补充了一个加法的重载。 4.std::visit现实operate<<.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <variant>
 
 // 请修复这个函数的定义：10 分
+template<class T>
 std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
     os << "{";
     for (size_t i = 0; i < a.size(); i++) {
@@ -16,19 +17,45 @@ std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
 
 // 请修复这个函数的定义：10 分
 template <class T1, class T2>
-std::vector<T0> operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
+auto operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
     // 请实现列表的逐元素加法！10 分
-    // 例如 {1, 2} + {3, 4} = {4, 6}
+    // 例如 {1, 2} + {3, 4} = {4, 6}12
+    using T0 = decltype(T1{} + T2{});
+    std::vector<T0> addVector;
+    size_t minLen = std::min(a.size(), b.size());
+    for (size_t i = 0; i < minLen; i++) {
+        addVector.push_back(a[i] + b[i]);
+    }
+    return addVector;
+
 }
 
 template <class T1, class T2>
 std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
     // 请实现自动匹配容器中具体类型的加法！10 分
+    std::variant<T1, T2> ret;
+    std::visit([&](auto const& t1, auto const& t2) {
+        ret = t1 + t2;
+    }, a, b);
+    return ret;  
+}
+
+template<class T1, class T2>
+std::variant<T1, T2> operator+(std::variant<T1, T2> const& a, T2 const& b) {
+    std::variant<T1, T2> ret;
+    std::visit([&](auto const& t) {
+        ret = t + b;
+    }, a);
+    return ret;
 }
 
 template <class T1, class T2>
 std::ostream &operator<<(std::ostream &os, std::variant<T1, T2> const &a) {
     // 请实现自动匹配容器中具体类型的打印！10 分
+    std::visit([&](auto const &t) {
+        os << t;
+    }, a);
+    return os;
 }
 
 int main() {
