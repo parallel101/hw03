f1556a69890f86741e6ca7e4438e1f33c7e4e351
GeLee-Q <865038696@qq.com>
2021-12-31 20:54:54 +0800

    hw03

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <variant>
 
 // 请修复这个函数的定义：10 分
+template<class T>
 std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
     os << "{";
     for (size_t i = 0; i < a.size(); i++) {
@@ -16,19 +17,68 @@ std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
 
 // 请修复这个函数的定义：10 分
 template <class T1, class T2>
-std::vector<T0> operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
+auto operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
     // 请实现列表的逐元素加法！10 分
     // 例如 {1, 2} + {3, 4} = {4, 6}
+    using T0 = decltype(T1{} + T2{});
+    const int n = std::min(a.size(),b.size());
+    std::vector<T0> res(n,0);
+    for(int i =0; i < n ; i++){
+        res[i] = a[i]+b[i];
+    }
+    return res;
 }
 
 template <class T1, class T2>
 std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
     // 请实现自动匹配容器中具体类型的加法！10 分
-}
+    return std::visit([&] (auto const &t1, auto const &t2)
+        ->std::variant<T1 ,T2>{
+        return  t1 + t2;
+    }, a, b);
+};
+
+
+
+template <class T1, class T2>
+auto operator+(std::variant<T1, T2> const &a, T2 const &b) {
+    std::variant<T1, T2> b1 = b;
+    return a + b1;
+};
+
+template <class T1, class T2, class T3>
+auto operator+(T3 const &a, std::variant<T1, T2> const &b) {
+    std::variant<T1, T2> a1 = a;
+    return  a1 + b;
+};
+
+//template <class T1, class T2>
+//auto operator+(std::variant<T1, T2> const &a, T2 const &b) {
+//    if(a.index() == 0){
+//        return std::get<T1>(a) + b;
+//    }else{
+//        return std::get<T2>(a) + b;
+//    }
+//};
+//
+//template <class T1, class T2, class T3>
+//auto operator+(T3 const &a, std::variant<T1, T2> const &b) {
+//    if(b.index() == 0){
+//        return a + std::get<T1>(b);
+//    }else{
+//        return a + std::get<T2>(b);
+//    }
+//};
+
+
 
 template <class T1, class T2>
 std::ostream &operator<<(std::ostream &os, std::variant<T1, T2> const &a) {
     // 请实现自动匹配容器中具体类型的打印！10 分
+    std::visit([&](auto const & it){
+        os << it << std::endl;
+    },a);
+    return os;
 }
 
 int main() {
