4da63ad40dffdcbe810aeb910e68113558056816
woAIxuexiSR <woaixuexisr>
2021-12-31 13:55:35 +0800

    finish hw03

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <variant>
 
 // 请修复这个函数的定义：10 分
+template <class T>
 std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
     os << "{";
     for (size_t i = 0; i < a.size(); i++) {
@@ -16,19 +17,38 @@ std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
 
 // 请修复这个函数的定义：10 分
 template <class T1, class T2>
-std::vector<T0> operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
-    // 请实现列表的逐元素加法！10 分
-    // 例如 {1, 2} + {3, 4} = {4, 6}
+auto operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
+    using T0 = decltype(T1{} + T2{});
+    std::vector<T0> ans;
+    for(size_t i = 0; i < std::min(a.size(), b.size()); ++i)
+        ans.push_back(a[i] + b[i]);
+    return ans;
+}
+
+template <class T1, class T2, class T>
+auto operator+(std::variant<T1, T2> const& a, T const& b)
+{
+    static_assert(std::is_same_v<T1, T> || std::is_same_v<T2, T>);
+    return std::visit([&](auto const& t1) -> std::variant<T1, T2> {
+        return t1 + b;
+    }, a);
 }
 
 template <class T1, class T2>
-std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
+auto operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
     // 请实现自动匹配容器中具体类型的加法！10 分
+    return std::visit([](auto const& t1, auto const& t2) -> std::variant<T1, T2> {
+        return t1 + t2;
+    }, a, b);
 }
 
 template <class T1, class T2>
 std::ostream &operator<<(std::ostream &os, std::variant<T1, T2> const &a) {
     // 请实现自动匹配容器中具体类型的打印！10 分
+    std::visit([&](auto const& t){
+        os << t << std::endl;
+    }, a);
+    return os;
 }
 
 int main() {
@@ -39,7 +59,7 @@ int main() {
     auto c = a + b;
 
     // 应该输出 1
-    std::cout << std::is_same_v<decltype(c), std::vector<double>> << std::endl;
+    std::cout << std::is_same_v<decltype(c), std::vector<double> > << std::endl;
 
     // 应该输出 {4.14, 6.718, 2.618}
     std::cout << c << std::endl;
