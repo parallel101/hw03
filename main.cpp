a24b5b7672a43d0d4b92c1c063219b6f4367895a
yangyueren <yangyueren@outlook.com>
2022-01-03 08:00:41 +0000

    [fix] improve

--- main.cpp
+++ main.cpp
@@ -20,43 +20,42 @@ std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
 
 // 请修复这个函数的定义：10 分
 template <class T1, class T2>
-decltype(auto) operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
+auto operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
     // 请实现列表的逐元素加法！10 分
     // 例如 {1, 2} + {3, 4} = {4, 6}
     using T0 = decltype(T1{} + T2{});
     std::vector<T0> res;
-    for(size_t i=0; i<a.size()&&i<b.size(); i++){
+    for(size_t i=0; i<std::min(a.size(),b.size()); i++){
         res.push_back(a[i]+b[i]);
     }
     return res;
 }
 
 template <class T1, class T2>
-decltype(auto) operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
+std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
     // 请实现自动匹配容器中具体类型的加法！10 分
-    return std::visit([&](auto const&x, auto const& y){
+    // 注意lambda要有返回值。
+    return std::visit([&](auto const&x, auto const& y)->std::variant<T1, T2> {
         return x + y;
     }, a, b);
 }
 
 template <class T1, class T2, class T3>
-decltype(auto) operator+(std::vector<T3> const &a, std::variant<T1, T2> const &b) {
+auto operator+(const T3&a, std::variant<T1, T2> const &b) {
     // 请实现自动匹配容器中具体类型的加法！10 分
-    return std::visit([&](auto const&y){
-        return a + y;
-    }, b);
+    std::variant<T1, T2> v_a = a; // 亮点。
+    return v_a + b;
 }
 
 template <class T1, class T2, class T3>
-decltype(auto) operator+(std::variant<T1, T2> const &a, std::vector<T3> const &b) {
+auto operator+(std::variant<T1, T2> const &a, const T3&b) {
     // 请实现自动匹配容器中具体类型的加法！10 分
-    return std::visit([&](auto const&x){
-        return x + b;
-    }, a);
+    std::variant<T1, T2> v_b = b;
+    return a + v_b;
 }
 
-template <class T1, class T2>
-std::ostream &operator<<(std::ostream &os, std::variant<T1, T2> const &a) {
+template <class T1, class ...ARGS>
+std::ostream &operator<<(std::ostream &os, std::variant<T1, ARGS...> const &a) {
     // 请实现自动匹配容器中具体类型的打印！10 分
     std::visit([&](auto const& x){
         os << x;
