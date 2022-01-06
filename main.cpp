0fc7f7c5841b5ecdf1f1f8ef9807884e2c35ab71
adelacvg <adelacvgaoiro@163.com>
2022-01-06 19:07:35 +0800

    update

--- main.cpp
+++ main.cpp
@@ -2,12 +2,23 @@
 #include <vector>
 #include <variant>
 #include <algorithm>
+#include <iterator>
+#include<ranges>
 
 // 请修复这个函数的定义：10 分
 template <class T>
 std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
     os << "{";
-    std::copy(std::begin(x), std::end(x),std::ostream_iterator(os, ", "));
+    // auto joined = a | std::view::transform([](int x) {return std::to_string(x);})
+    //                 | std::view::join(',');
+    // std::cout << std::to_string(joined);
+    // std::copy(std::begin(a), std::end(a),std::ostream_iterator<T>(os, ", "));
+    for(int i=0;i<a.size();i++)
+    {
+        os<<a[i];
+        if(i!=a.size()-1)
+        os<<", ";
+    }
     os << "}";
     return os;
 }
@@ -19,39 +30,33 @@ auto operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
     // 例如 {1, 2} + {3, 4} = {4, 6}
     using T0=decltype(T1{}+T2{});
     std::vector<T0> c;
-    std::transform (a.begin(), a.end(), b.begin(), std::back_inserter(c), std::plus<int>());
+    auto sz = std::min(a.size(),b.size());
+    std::transform (begin(a), next(begin(a),sz), begin(b), std::back_inserter(c), [](auto x,auto y){return x+y;});
     return c;
 }
 
-template <class T1, class T2>
-std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, T1 const &b)
+template <
+    class T1, class T2, class T3, 
+    std::enable_if_t<std::is_same_v<T1,T3>||std::is_same_v<T2,T3>,bool> = true
+>
+std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, T3 const &b)
 {
-    return std::static_cast<std::variant<T1, T2>>(b)+a;
+    return static_cast<std::variant<T1, T2>>(b)+a;
 }
 
-template <class T1, class T2>
-std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, T2 const &b)
+template <
+    class T1, class T2, class T3, 
+    std::enable_if_t<std::is_same_v<T1,T3>||std::is_same_v<T2,T3>,bool> = true
+>
+std::variant<T1, T2> operator+(T3 const &a, std::variant<T1, T2> const &b)
 {
-    return std::static_cast<std::variant<T1, T2>>(b)+a;
-}
-
-
-template <class T1, class T2>
-std::variant<T1, T2> operator+(T1 const &a, std::variant<T1, T2> const &b)
-{
-    return std::static_cast<std::variant<T1, T2>>(a)+b;
-}
-
-template <class T1, class T2>
-std::variant<T1, T2> operator+(T2 const &a, std::variant<T1, T2> const &b)
-{
-    return std::static_cast<std::variant<T1, T2>>(a)+b;
+    return static_cast<std::variant<T1, T2>>(a)+b;
 }
 
 template <class T1, class T2>
 std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
     // 请实现自动匹配容器中具体类型的加法！10 分
-    return std::visit([](auto const& x,auto const& y){return x+y;},a,b);
+    return std::visit([](auto const& x,auto const& y){return std::variant<T1,T2>{x+y};},a,b);
 }
 
 // template <class T1, class... T2>
@@ -63,12 +68,10 @@ std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, std::variant<T1, T
 template <class T1, class T2>
 std::ostream &operator<<(std::ostream &os, std::variant<T1, T2> const &a) {
     // 请实现自动匹配容器中具体类型的打印！10 分
-    return std::visit(
-        [](auto const& x){
-            return os<<x;
-        },
-        a        
-    );
+    std::visit([&] (auto const &x) {
+        os << x;
+    }, a);
+    return os;
 }
 
 int main() {
