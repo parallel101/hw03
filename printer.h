5d67bcd0398e6abfec672cccb64f9a5ed28c80f1
Derived Cat <hooyuser@outlook.com>
2022-01-08 18:18:56 +0800

    use std::begin()

--- printer.h
+++ printer.h
@@ -22,38 +22,25 @@ template<typename UnknownType, typename ReferenceType>
 concept ConstructibleFrom = ExplicitlyConvertibleTo<ReferenceType, std::decay_t<UnknownType>>;
 
 template<typename UnknownType>
-concept BuiltinArray = std::is_array_v<std::remove_cvref_t<UnknownType>>;
-
-template<typename UnknownType>
 concept Advanceable = requires(UnknownType x) { ++x; };
 
 template<typename UnknownType>  //可迭代对象约束，包括普通数组和任意提供以下迭代接口的对象
-concept Iterable = BuiltinArray<UnknownType> || requires(UnknownType x) {
-	{ x.begin() }->Advanceable;
-	{ *x.begin() }->AnyBut<void>;
-	{ x.begin() != x.end() }->ExplicitlyConvertibleTo<bool>;
+concept Iterable = requires(UnknownType x) {
+	{ std::begin(x) }->Advanceable;
+	{ *std::begin(x) }->AnyBut<void>;
+	{ std::begin(x) != std::end(x) }->ExplicitlyConvertibleTo<bool>;
 };
 
 // 请修复这个函数的定义：10 分
 // 支持打印一般的可迭代对象
 auto& operator<<(SubtypeOf<std::ostream> auto& printer, Iterable auto&& container) requires (
-	(requires { printer << *container.begin(); } ||    //要求 printer 与 begin() 接口兼容
-		requires { printer << container[0]; }) && !ConstructibleFrom<decltype(container), const char*>)  //或者 printer 与数组元素兼容，但有特例：字符串不使用该重载！
-{
-	auto [Startpoint, Endpoint] = [&] {
-		if constexpr (BuiltinArray<decltype(container)>) {
-			return std::tuple{ container, container + sizeof(container) / sizeof(container[0]) };
-		}
-		else {
-			return std::tuple{ container.begin(), container.end() };
-		}
-	}();
+	requires { printer << *std::begin(container); } && !ConstructibleFrom<decltype(container), const char*>) {   //要求 printer 与 begin() 接口兼容，但有特例：字符串不使用该重载！
+
 	std::string_view separator = "";
-	printer << "{";
-	for (; Startpoint != Endpoint; ++Startpoint) {
+	printer << "[";
+	for (auto Startpoint = std::begin(container); Startpoint != std::end(container); ++Startpoint)
 		printer << std::exchange(separator, ", ") << *Startpoint;
-	}
-	printer << "}";
+	printer << "]";
 	return printer;
 }
 
