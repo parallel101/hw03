b88c4a8cedf79c19e1c9379c506c7488ed60ade3
QifanWang <one302755067@gmail.com>
2022-11-24 19:49:51 +0800

    finish hw03

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <variant>
+#include <cpp_type_name.h>
 
 // 请修复这个函数的定义：10 分
+template<typename T>
 std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
     os << "{";
     for (size_t i = 0; i < a.size(); i++) {
@@ -16,19 +18,45 @@ std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
 
 // 请修复这个函数的定义：10 分
 template <class T1, class T2>
-std::vector<T0> operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
+std::vector<decltype(T1{} + T2{})>
+operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
     // 请实现列表的逐元素加法！10 分
     // 例如 {1, 2} + {3, 4} = {4, 6}
+    // 当列表不等长时，取较短的
+    size_t len = std::min(a.size(), b.size());
+    std::vector<decltype(T1{} + T2{})> ret;
+    for (size_t i = 0; i < len; ++i)
+        ret.push_back(a[i] + b[i]);
+    return ret;
 }
 
-template <class T1, class T2>
-std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
+template <class T1, class T2, class U>
+std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, U &&b) {
     // 请实现自动匹配容器中具体类型的加法！10 分
+    using decay_U = std::decay_t<U>;
+    if constexpr (std::is_same_v<T1, decay_U> || std::is_same_v<T2, decay_U>)  {
+        return std::visit([&b](auto &&A) -> std::variant<T1, T2> {
+            return A + b;
+        }, a);
+    } else if constexpr (std::is_same_v<std::variant<T1, T2>, decay_U>) {
+        return std::visit([](auto &&A, auto &&B) -> std::variant<T1, T2> {
+            return A + B;
+        }, a, b);
+    } else {
+        std::cout << cpp_type_name<std::variant<T1, T2>>() << std::endl;
+        std::cout << cpp_type_name<U>() << std::endl;
+        throw std::bad_variant_access{};
+    }
+    
 }
 
 template <class T1, class T2>
 std::ostream &operator<<(std::ostream &os, std::variant<T1, T2> const &a) {
     // 请实现自动匹配容器中具体类型的打印！10 分
+    std::visit([&os](auto &&val) {
+        os << val;
+    }, a);
+    return os;
 }
 
 int main() {
