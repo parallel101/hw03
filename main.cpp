5196095a53d6335163aff2474220dca13db39388
xiao-ming-hub <3154802372@qq.com>
2023-01-25 11:09:59 +0800

    finish hw3

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <variant>
 
 // 请修复这个函数的定义：10 分
+template <class T>
 std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
     os << "{";
     for (size_t i = 0; i < a.size(); i++) {
@@ -16,19 +17,34 @@ std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
 
 // 请修复这个函数的定义：10 分
 template <class T1, class T2>
-std::vector<T0> operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
+auto operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
     // 请实现列表的逐元素加法！10 分
     // 例如 {1, 2} + {3, 4} = {4, 6}
+    std::vector<decltype(T1() + T2())> c(std::min(a.size(), b.size()));
+    for (int i = 0; i < c.size(); i++) c[i] = a[i] + b[i];
+    return c;
 }
 
-template <class T1, class T2>
-std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
+template <class T1, class T2, class T3>
+auto operator+(std::variant<T1, T2> const &a, T3 const &b) {
     // 请实现自动匹配容器中具体类型的加法！10 分
+    return std::visit([&](auto const &x) {
+        return x + b; }, a);
+}
+
+template <class T1, class T2, class T3>
+auto operator+(T1 const &a, std::variant<T2, T3> const &b) {
+    return std::visit([&](auto const &x) {
+        return a + x; }, b);
 }
 
 template <class T1, class T2>
 std::ostream &operator<<(std::ostream &os, std::variant<T1, T2> const &a) {
     // 请实现自动匹配容器中具体类型的打印！10 分
+    return std::visit([&](auto const &x) -> std::ostream & {
+        os << x;
+        return os;
+    }, a);
 }
 
 int main() {
