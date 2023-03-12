99447da9161ffb280eaef1279528a1c293a37ced
WangYuHang-cmd <377374481@qq.com>
2023-03-12 18:52:40 +0800

    Finished basic demand

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <variant>
 
 // 请修复这个函数的定义：10 分
+template<class T>
 std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
     os << "{";
     for (size_t i = 0; i < a.size(); i++) {
@@ -16,26 +17,63 @@ std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
 
 // 请修复这个函数的定义：10 分
 template <class T1, class T2>
-std::vector<T0> operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
+auto operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
+    using T0=decltype(T1{}+T2{});
     // 请实现列表的逐元素加法！10 分
+    int _=std::min(a.size(), b.size());
+    std::vector<T0> vec(_);
+    for(int i=0;i<_;++i){
+        vec[i]=(a[i]+b[i]);
+    }
+    // for(auto u:vec) std::cout << u << " " ;std::cout<<"\n";
+    return vec;
     // 例如 {1, 2} + {3, 4} = {4, 6}
 }
 
 template <class T1, class T2>
 std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
     // 请实现自动匹配容器中具体类型的加法！10 分
+    std::variant<T1, T2> res;
+    std::visit([&](auto const& A, auto const& B){
+        res=A+B;
+    }, a, b);
+    return res;
+}
+
+template<class T1,class T2>
+std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, T1 const &b) {
+    // 请实现自动匹配容器中具体类型的加法！10 分
+    std::variant<T1, T2> res;
+    if(std::holds_alternative<T1>(a)){
+        res=std::get<0>(a)+b;
+    }
+    return res;
+}
+
+template<class T1,class T2>
+std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, T2 const &b) {
+    // 请实现自动匹配容器中具体类型的加法！10 分
+    std::variant<T1, T2> res;
+    if(std::holds_alternative<T2>(a)){
+        res=std::get<1>(a)+b;        
+    }
+    return res;
 }
 
 template <class T1, class T2>
 std::ostream &operator<<(std::ostream &os, std::variant<T1, T2> const &a) {
     // 请实现自动匹配容器中具体类型的打印！10 分
+    std::visit([&](auto const &t){
+        os << t;
+    }, a);
+    return os;
 }
 
 int main() {
     std::vector<int> a = {1, 4, 2, 8, 5, 7};
-    std::cout << a << std::endl;
+    std::cout <<  a << std::endl;
     std::vector<double> b = {3.14, 2.718, 0.618};
-    std::cout << b << std::endl;
+    std::cout <<  b << std::endl;
     auto c = a + b;
 
     // 应该输出 1
